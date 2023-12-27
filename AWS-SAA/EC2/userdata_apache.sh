260720f9a765a1fe0ddfaf3102e6a0290f64cbb2
aakkiiff <jackakif@gmail.com>
2023-12-27 19:31:43 +0600

    typo fixed

--- AWS-SAA/EC2/userdata_apache.sh
+++ AWS-SAA/EC2/userdata_apache.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 apt update -y
-apt install nginx -y
-systemctl start nginx
-systemctl enable nginx
+apt install apache2 -y
+systemctl start apache2
+systemctl enable apache2
+echo "<h1>Deployed via aws userdata</h1>" > /var/www/html/index.html
--- AWS-SAA/EC2/userdata_nginx.sh
+++ AWS-SAA/EC2/userdata_nginx.sh
@@ -1,6 +1,5 @@
 #!bin/bash
 apt update -y
-apt install apache2 -y
-systemctl start apache2
-systemctl enable apache2
-echo "<h1>Deployed via aws userdata</h1>" > /var/www/html/index.html
+apt install nginx -y
+systemctl start nginx
+systemctl enable nginx
