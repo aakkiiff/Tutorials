1c6656fc3cf8b8a1f74d9dc90d060d96e7432aec
akif <104318101+aakkiiff@users.noreply.github.com>
2024-03-12 21:35:10 +0600

    Update userdata_apache.sh

--- AWS-SAA/EC2/userdata_apache.sh
+++ AWS-SAA/EC2/userdata_apache.sh
@@ -4,3 +4,4 @@ apt install apache2 -y
 systemctl start apache2
 systemctl enable apache2
 echo "<h1>Deployed via aws userdata</h1>" > /var/www/html/index.html
+echo "<h1>Hostname is $(hostname -f)</h1>" >> /var/www/html/index.html
