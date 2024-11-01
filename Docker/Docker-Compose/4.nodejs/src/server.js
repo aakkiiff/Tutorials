f21cfb3396b15357db48ebb66bc2b2edd4cb7b8b
akif <jackakif@gmail.com>
2024-11-01 23:47:26 +0600

    reformation

--- Docker/Docker-Compose/4.nodejs/src/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-console.log('Server Listening on 8080');
-http.createServer(function (req, res) {
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-  
-}).listen(8080);
