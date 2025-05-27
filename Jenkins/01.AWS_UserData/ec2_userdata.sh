ffbc135c7bd92533e5c67200e108e7efedf85204
akif <104318101+aakkiiff@users.noreply.github.com>
2025-05-27 22:31:38 +0600

    updated

--- Jenkins/01.AWS_UserData/ec2_userdata.sh
+++ Jenkins/01.AWS_UserData/ec2_userdata.sh
@@ -3,7 +3,7 @@ sudo apt update -y
 sudo apt install openjdk-17-jre -y 
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null sudo 
-apt-get update -y 
+sudo apt-get update -y 
 sudo apt-get install jenkins -y
 
 sudo apt install nginx -y
