22ed7873edfed1bc256d56fa6098384460428c1c
aakkiiff <akif22inbox@gmail.com>
2026-03-09 01:34:38 +0600

    s3 updated

--- Terraform/Practice/s3-website/main.tf
+++ Terraform/Practice/s3-website/main.tf
@@ -54,24 +54,24 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 ############################
 # BUCKET POLICY (PUBLIC READ)
 ############################
-# resource "aws_s3_bucket_policy" "public_policy" {
-#   bucket = aws_s3_bucket.website_bucket.id
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
 
-#   depends_on = [aws_s3_bucket_public_access_block.public_access]
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid       = "PublicReadGetObject"
-#         Effect    = "Allow"
-#         Principal = "*"
-#         Action    = "s3:GetObject"
-#         Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
 
 ############################
 # UPLOAD INDEX.HTML
